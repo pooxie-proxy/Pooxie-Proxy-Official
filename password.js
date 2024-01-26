ed0772b93488e6c55a6bef4ecbca5677fe5b5353
Pooxie Proxy Official <149209837+pooxie-proxy@users.noreply.github.com>
2024-01-25 18:15:52 -0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,29 @@
+    var users = {
+      "pooxieuser": "iamnotafuckingitadmin",
+      "pooxieadmin": "sunscreenmassproduction",
+      // Add more users as needed
+    };
+
+    function authenticateUser() {
+      var username = prompt("What is the formula for finding the area of triangles (U)");
+      var password = prompt("What is the formula for finding the volume of a mass of rectangular prisms (P)");
+
+      if (username === null || password === null || users[username] !== password) {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      } else {
+        alert("Login successful!");
+      }
+    }
+
+    function checkForAuthentication() {
+      if (!localStorage.getItem("authenticated")) {
+        authenticateUser();
+      }
+    }
+
+    window.onload = checkForAuthentication;
+
+    window.onbeforeunload = function () {
+      // Save a flag in local storage indicating that the user has been authenticated
+      localStorage.setItem("authenticated", "true");
+    };
