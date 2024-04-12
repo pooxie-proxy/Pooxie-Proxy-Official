141c3ea2dbea4ec3ab94ec658c4bbef679315037
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2024-04-12 07:33:19 -0600

    Create cookies.js

---
+++ static/scripts/cookies.js
@@ -0,0 +1,23 @@
+// Function to get the value of a cookie
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Check if the cookie exists
+var myCookie = getCookie("loggedin");
+if (myCookie == "") {
+    // Redirect to index.html if the cookie doesn't exist
+    window.location.href = "index.html";
+}
